024620e873aaa5e58e74fff205f75461bfc07435
Andréa BIGOT <70588437+l3miage-bigotan@users.noreply.github.com>
2022-03-05 17:54:44 +0100

    TeacherTest

--- src/test/java/fr/uga/im2ag/l3/miage/db/repository/TeacherTest.java
+++ src/test/java/fr/uga/im2ag/l3/miage/db/repository/TeacherTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class TeacherTest extends Base {
 
     TeacherRepository teacherRepository;
@@ -24,10 +26,43 @@ class TeacherTest extends Base {
     @Test
     void shouldSaveTeacher()  {
         // TODO
+        final var classe = Fixtures.createClass();
+        final var subject = Fixtures.createSubject();
+        final var student =Fixtures.createStudent(classe);
+
+        final var teacher = Fixtures.createTeacher(subject,classe,student);
+        entityManager.getTransaction().begin();
+        entityManager.persist(classe);
+        entityManager.persist(subject);
+        entityManager.persist(student);
+
+        teacherRepository.save(teacher);
+        entityManager.getTransaction().commit();
+        entityManager.detach(teacher);
+
+        var pTeacher = teacherRepository.findById(teacher.getId());
+        assertThat(pTeacher).isNotNull().isNotSameAs(teacher);
+        assertThat(pTeacher.getLastName()).isEqualTo(teacher.getLastName());
     }
 
     @Test
     void shouldFindHeadingGraduationClassByYearAndName() {
+        final var classe = Fixtures.createClass();
+        final var subject = Fixtures.createSubject();
+        final var student =Fixtures.createStudent(classe);
+
+        final var teacher = Fixtures.createTeacher(subject,classe,student);
+        entityManager.getTransaction().begin();
+        entityManager.persist(classe);
+        entityManager.persist(subject);
+        entityManager.persist(student);
+
+        teacherRepository.save(teacher);
+        entityManager.getTransaction().commit();
+        entityManager.detach(teacher);
+        var pTeacher = teacherRepository.findHeadingGraduationClassByYearAndName(classe.getYear(), classe.getName());
+        assertThat(pTeacher).isNotNull().isNotSameAs(teacher);
+        assertThat(pTeacher.getLastName()).isEqualTo(teacher.getLastName());
         // TODO
     }
 
