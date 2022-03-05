603683a3504349dc84765905ff083b4bc4f92fdc
Andréa BIGOT <70588437+l3miage-bigotan@users.noreply.github.com>
2022-03-05 17:47:35 +0100

    GradeRepository

--- src/main/java/fr/uga/im2ag/l3/miage/db/repository/impl/GradeRepositoryImpl.java
+++ src/main/java/fr/uga/im2ag/l3/miage/db/repository/impl/GradeRepositoryImpl.java
@@ -1,10 +1,12 @@
 package fr.uga.im2ag.l3.miage.db.repository.impl;
 
+import fr.uga.im2ag.l3.miage.db.model.Student;
 import fr.uga.im2ag.l3.miage.db.repository.api.GradeRepository;
 import fr.uga.im2ag.l3.miage.db.model.Grade;
 import fr.uga.im2ag.l3.miage.db.model.Subject;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 public class GradeRepositoryImpl extends BaseRepositoryImpl implements GradeRepository {
@@ -21,13 +23,14 @@ public class GradeRepositoryImpl extends BaseRepositoryImpl implements GradeRepo
     @Override
     public List<Grade> findHighestGrades(int limit) {
         // TODO
-        return null;
+        return  entityManager.createQuery("Select g from Grade g order by grade desc ").setMaxResults(limit).getResultList();
+
     }
 
     @Override
     public List<Grade> findHighestGradesBySubject(int limit, Subject subject) {
         // TODO
-        return null;
+        return  entityManager.createQuery("Select g from Grade g where subject_id = "+ subject.getId()+" order by grade desc ").setMaxResults(limit).getResultList();
     }
 
     @Override
@@ -42,12 +45,12 @@ public class GradeRepositoryImpl extends BaseRepositoryImpl implements GradeRepo
 
     @Override
     public Grade findById(Long id) {
-        return null;
+        return entityManager.find(Grade.class,id);
     }
 
     @Override
     public List<Grade> getAll() {
         // TODO
-        return null;
+        return entityManager.createNamedQuery("Student.getAll").getResultList();
     }
 }
