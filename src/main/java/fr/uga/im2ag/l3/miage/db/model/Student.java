02df5cef8a19cdbe0967e4e72aa537b88b3113b9
Andréa BIGOT <70588437+l3miage-bigotan@users.noreply.github.com>
2022-03-05 17:44:21 +0100

    Student

--- src/main/java/fr/uga/im2ag/l3/miage/db/model/Student.java
+++ src/main/java/fr/uga/im2ag/l3/miage/db/model/Student.java
@@ -2,10 +2,17 @@ package fr.uga.im2ag.l3.miage.db.model;
 
 import java.util.List;
 
-// TODO ajouter une named query pour une des requêtes à faire dans le repository
-public class Student extends Person {
+import javax.persistence.*;
 
+// TODO ajouter une named query pour une des requÃªtes Ã  faire dans le repository
+@NamedQuery(name="Student.findAll", query="SELECT s FROM Student s")
+
+@Entity
+@DiscriminatorValue("student")
+public class Student extends Person {
+	@ManyToOne
     private GraduationClass belongTo;
+    @OneToMany
     private List<Grade> grades;
 
     public GraduationClass getBelongTo() {
@@ -26,3 +33,4 @@ public class Student extends Person {
         return this;
     }
 }
+
